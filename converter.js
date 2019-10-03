74df643d7ed494eb8adc456b9d040b8aa0b03da1
David M <davidmosk@gmail.com>
2019-10-03 18:32:43 +0800

    hex to rgb converter working v1

--- converter.js
+++ converter.js
@@ -1,3 +1,10 @@
-module.exports.myConverter= function(hex){
-	return rgb;
+module.exports.convertHextoRGB= function(hex){
+// take in hex 
+    let bigint = parseInt(hex, 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+
+    return 'rgb( ' + r + "," + g + "," + b + ' )';
+
 }
