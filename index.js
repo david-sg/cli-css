8d7000ff3141ec8c96c3e5e6f1378d61267ce189
David M <davidmosk@gmail.com>
2019-10-04 15:17:15 +0800

    color output of variable

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ let b = null;
 let g = null;
 let r = null;
 console.log('original:' + process.argv[2]);
+const output = {};
 
 myConverter.convertHextoRGB(process.argv[2]);
+
+console.log('r is:' + myConverter.r );
+
+let runFunction = myConverter.convertHextoRGB(process.argv[2]);
+
+console.log('r is:' + runFunction.r );
+console.log('g is:' + runFunction.g );
+console.log('b is:' + runFunction.b );
 
 // console.log('rgb( ' + r + "," + g + "," + b + ' )');
 
@@ -14,4 +23,4 @@ myConverter.convertHextoRGB(process.argv[2]);
 // let myOutput= myConverter.convertHextoRGB(process.argv[2]);
 // console.log(myOutput);
 // color the output
-// console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, myOutput);
+console.log(`\x1b[38;2;${runFunction.r};${runFunction.g};${runFunction.b}m%s\x1b[0m`, process.argv[2]);
